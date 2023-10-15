61579a384bbc0bef54c25f2a79723edcd78cfa7f
Cy Dixon <94246986+cylissama@users.noreply.github.com>
2023-10-15 18:31:51 -0500

    Update admin.py

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from .models import Quiz, Question, Choice, QuizTaker
 
+#registered in the admin page
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Choice)
